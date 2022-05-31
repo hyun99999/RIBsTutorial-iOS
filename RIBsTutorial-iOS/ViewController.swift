d28e0596b12f2400ce0c80891c516340df3c12f4
hyun99999 <ypsophila97@icloud.com>
2022-05-31 15:22:40 +0900

    chore: 프로젝트 세팅

---
+++ RIBsTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsTutorial-iOS
+//
+//  Created by kimhyungyu on 2022/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
