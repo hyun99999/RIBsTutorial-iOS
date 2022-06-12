bf7526cbdf78c71f0ef31eef83b171b00c8c062a
hyun99999 <ypsophila97@icloud.com>
2022-06-12 23:08:32 +0900

    feat: Root Router 가 LoggedIn RIB 를 build 할 수 있도록 함 (#2)
    - 정상적으로 RIB 를 dismiss 하고 detach 할 수 있게 되었습니다.

--- tutorial2/TicTacToe/Root/RootRouter.swift
+++ tutorial2/TicTacToe/Root/RootRouter.swift
@@ -16,13 +16,14 @@
 
 import RIBs
 
-protocol RootInteractable: Interactable, LoggedOutListener {
+protocol RootInteractable: Interactable, LoggedOutListener, LoggedInListener {
     var router: RootRouting? { get set }
     var listener: RootListener? { get set }
 }
 
 protocol RootViewControllable: ViewControllable {
     func present(viewController: ViewControllable)
+    func dismiss(viewController: ViewControllable)
 }
 
 final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, RootRouting {
@@ -41,6 +42,20 @@ final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, Ro
         super.didLoad()
 
         routeToLoggedOut()
+    }
+    
+    // MARK: - RootRouting
+
+    func routeToLoggedIn(withPlayer1Name player1Name: String, player2Name: String) {
+        // Detach LoggedOut RIB.
+        if let loggedOut = self.loggedOut {
+            detachChild(loggedOut)
+            viewController.dismiss(viewController: loggedOut.viewControllable)
+            self.loggedOut = nil
+        }
+
+        let loggedIn = loggedInBuilder.build(withListener: interactor)
+        attachChild(loggedIn)
     }
 
     // MARK: - Private
--- tutorial2/TicTacToe/Root/RootViewController.swift
+++ tutorial2/TicTacToe/Root/RootViewController.swift
@@ -47,4 +47,10 @@ final class RootViewController: UIViewController, RootPresentable, RootViewContr
     func present(viewController: ViewControllable) {
         present(viewController.uiviewController, animated: true, completion: nil)
     }
+    
+    func dismiss(viewController: ViewControllable) {
+        if presentedViewController === viewController.uiviewController {
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
