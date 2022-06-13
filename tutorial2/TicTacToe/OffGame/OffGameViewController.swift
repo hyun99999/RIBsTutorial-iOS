4907cf13d064a5213ab5f7dde8261929e521cf9e
hyun99999 <ypsophila97@icloud.com>
2022-06-14 00:18:39 +0900

    feat: OffGameViewController UI 구성 (#2)

--- tutorial2/TicTacToe/OffGame/OffGameViewController.swift
+++ tutorial2/TicTacToe/OffGame/OffGameViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import RIBs
-import RxSwift
+import SnapKit
 import UIKit
 
-protocol OffGamePresentableListener: AnyObject {
+protocol OffGamePresentableListener: class {
     // TODO: Declare properties and methods that the view controller can invoke to perform
     // business logic, such as signIn(). This protocol is implemented by the corresponding
     // interactor class.
 }
 
 final class OffGameViewController: UIViewController, OffGamePresentable, OffGameViewControllable {
+    var uiviewController: UIViewController {
+        return self
+    }
 
     weak var listener: OffGamePresentableListener?
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Method is not supported")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.yellow
+        buildStartButton()
+    }
+
+    // MARK: - Private
+
+    private func buildStartButton() {
+        let startButton = UIButton()
+        view.addSubview(startButton)
+        startButton.snp.makeConstraints { (maker: ConstraintMaker) in
+            maker.center.equalTo(self.view.snp.center)
+            maker.leading.trailing.equalTo(self.view).inset(40)
+            maker.height.equalTo(100)
+        }
+        startButton.setTitle("Start Game", for: .normal)
+        startButton.setTitleColor(UIColor.white, for: .normal)
+        startButton.backgroundColor = UIColor.black
+    }
 }
